93eb75c5ba48453429417f08ec445195d95fce6e
Kian Saeidnia <ksaeidnia06@gmail.com>
2025-08-29 18:05:13 +1000

    Basic structure to user controller

--- src/main/java/com/cab302/cab302/controller/UserController.java
+++ src/main/java/com/cab302/cab302/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.cab302.cab302.controller;
 
+import javafx.fxml.FXML;
+
 public class UserController {
+
+    @FXML
+    private void onSignup() {
+
+        // TODO: Add data validation
+        // TODO: Password hashing
+        // TODO: Add account to db
+
+    }
+
+    @FXML
+    private void onDeleteAccount() {
+
+        // TODO: Add account to db
+        
+    }
+
+    @FXML
+    private void editEditAccount() {
+        
+        // TODO: Add data validation
+        // TODO: Add account to db
+
+    }
 }
