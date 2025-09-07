eb7e340045a8cafc580f255bd01eebeabe949ae6
Don Jaison Thomas <donjaison100@gmail.com>
2025-09-07 04:01:31 +0300

    fixed errors

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,19 @@
 module com.cab302.cab302 {
+    // JavaFX
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.sql;
 
+    // JDBC
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
+
+    // Open packages for FXML reflection
     opens com.cab302.cab302 to javafx.fxml;
     opens com.cab302.cab302.controller to javafx.fxml;
+    opens com.cab302.cab302.Database to javafx.fxml;
 
+    // Exported packages
     exports com.cab302.cab302;
     exports com.cab302.cab302.controller;
+    exports com.cab302.cab302.Database;
 }
