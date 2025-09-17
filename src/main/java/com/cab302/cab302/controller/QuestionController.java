bbc8d6352564d04e3a16dacc80c6276c1e7cceb0
Kian Saeidnia <ksaeidnia06@gmail.com>
2025-09-17 10:26:33 +1000

    code cleanup

--- src/main/java/com/cab302/cab302/controller/QuestionController.java
+++ src/main/java/com/cab302/cab302/controller/QuestionController.java
@@ -1,17 +1,8 @@
 package com.cab302.cab302.controller;
 
-import com.cab302.cab302.Main;
-import com.cab302.cab302.model.UserAccount;
-import com.cab302.cab302.TestUserList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class QuestionController {
     @FXML
