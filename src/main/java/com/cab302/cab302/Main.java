37e16d4052038457fba136afb69276645bb16462
Kian Saeidnia <ksaeidnia06@gmail.com>
2025-09-18 20:51:01 +1000

    Made auth pages use preferred size on init start

--- src/main/java/com/cab302/cab302/Main.java
+++ src/main/java/com/cab302/cab302/Main.java
@@ -7,13 +7,11 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     public static final String TITLE = "Sign In / Log In";
-    public static final int WIDTH = 560;
-    public static final int HEIGHT = 420;
 
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("auth-view.fxml"));
-        Scene scene = new Scene(loader.load(), WIDTH, HEIGHT);
+        Scene scene = new Scene(loader.load());
         stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
