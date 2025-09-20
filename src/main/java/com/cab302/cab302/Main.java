ac5ee4e0f2aefaac33b4ba4672e0c00903ce697c
WinlosL <n11634669@qut.edu.au>
2025-09-20 13:45:00 +1000

    Start with login page

--- src/main/java/com/cab302/cab302/Main.java
+++ src/main/java/com/cab302/cab302/Main.java
@@ -16,8 +16,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         this.primaryStage = stage;
-//        FXMLLoader loader = new FXMLLoader(Main.class.getResource("Auth/signup-view.fxml"));
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("home-view.fxml"));
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("Auth/login-view.fxml"));
         Scene scene = new Scene(loader.load());
         stage.setTitle(TITLE);
         stage.setScene(scene);
