4662dd8a7a77f1fb3c0a280ed7aad92024177639
Don Jaison Thomas <donjaison100@gmail.com>
2025-09-21 08:47:34 +0300

    back button added for navigation

--- src/main/java/com/cab302/cab302/controller/ProfileController.java
+++ src/main/java/com/cab302/cab302/controller/ProfileController.java
@@ -5,7 +5,9 @@ import com.cab302.cab302.model.UserAccount;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -76,6 +78,20 @@ public class ProfileController {
             }
         });
     }
+    @FXML
+    private void goBackHome(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/cab302/cab302/home-view.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @FXML
     private void onChangePassword() {
